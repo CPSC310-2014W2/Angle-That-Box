259ee1f02839a6272d1832e823d657bf090f0e26
Mark Sayson <masayson@gmail.com>
2015-03-06 20:27:40 -0800

    Fix to user login, account creation
    Detail:
    * userExists result was not propagated to functions
    * Moved control flow into login, creation functions, verified working

--- js/factories/userFactory.js
+++ js/factories/userFactory.js
@@ -9,12 +9,18 @@ app.factory('UserFactory', function($firebase, $location)
          if (error) {
             console.log("Login failed", error);
          } else {
-            if (factory.userExists(authData.google.id) == false) {
-               alert("User not registered with app");
-            } else {
-               console.log("User authenticated, logging in");
-               factory.openDashboardView($scope);
-            }
+            factory.loginIfUserExists($scope, authData.google.id);
+         }
+      });
+   }
+   
+   factory.loginIfUserExists = function($scope, uid) {
+      ref.child(uid).once('value', function(data) {
+         if (data.val() === null) {
+            alert("User not registered with app");
+         } else {
+            console.log("User authenticated, logging in");
+            factory.openDashboardView($scope);
          }
       });
    }
@@ -26,38 +32,33 @@ app.factory('UserFactory', function($firebase, $location)
          alert("Please enter a birthdate");
       } else {
          ref.authWithOAuthPopup("google", function(error, authData) {
-            if (factory.isNewUserValid(error, authData)) {
-               console.log("Creating new user", authData.google.id);
-               factory.addUser(authData.google.id, "google", name, birthDate);
-               console.log("User successfully created, logging in");
-               factory.openDashboardView($scope);
+            if (error) {
+               console.log("Google authentication failed");
+            } else {
+               factory.addUserIfDoesNotExist($scope, authData.google.id, name, birthDate);
             }
          });
       }
    };
+   
+   factory.addUserIfDoesNotExist = function($scope, uid, name, birthDate) {
+      ref.child(uid).once('value', function(data) {
+         if (data.val() !== null) {
+            alert("Email already registered with app");
+         } else {
+            console.log("Creating new user", uid);
+            factory.addUser(uid, "google", name, birthDate);
+            console.log("User created, logging in");
+            factory.openDashboardView($scope);
+         }
+      });
+   }
    
    factory.addUser = function(uid, authProvider, name, birthDate) {
       ref.child(uid).set({
          "name": name,
          "birthdate": birthDate.valueOf(),
          "authProvider": authProvider
-      });
-   }
-   
-   factory.isNewUserValid = function(error, authData) {
-      if (error) {
-         console.log("Login failed!", error);
-      } else if (factory.userExists(authData.google.id)) {
-         alert("Email already registered with app");
-      } else {
-         return true;
-      }
-      return false;
-   }
-   
-   factory.userExists = function(uid) {
-      ref.child(uid).once('value', function(data) {
-         return data.val() != null;
       });
    }
    
