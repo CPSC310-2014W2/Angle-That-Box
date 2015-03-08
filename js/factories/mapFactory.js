9910806d852cb8371135aed0765be90be2711f1e
u1d8 <ryan.io.93@gmail.com>
2015-03-07 20:30:16 -0800

    Added banner for login page and map now adjusts based on markers

--- js/factories/mapFactory.js
+++ js/factories/mapFactory.js
@@ -1,28 +1,24 @@
 app.factory('MapFactory', function($http) 
 {
 	var factory = {};
-	var default_long = 49.3; //needs to change in the future
-	var default_lat = -123;
-	var default_zoom = 9;
 
 	factory.createMap = function() {
-	   var mapOptions = {
-		    zoom: default_zoom,
-		    center: new google.maps.LatLng(default_long, default_lat) //needs to change based on markers
-		}
-		return new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		return new google.maps.Map(document.getElementById('map-canvas'));
 	}
 
 	factory.getMapData = function(amap, markers) { //this just uses the sample json file for now instead of querying firebase
-		$http.get('sampleData.json').success(function(data) { 
+		$http.get('sampleData.json').success(function(data) {
+			var bounds = new google.maps.LatLngBounds(); 
 		    for (var key in data.locations) {
 		    	var loc = data.locations[key];
-			    createMarker(loc, amap, markers);
+		    	bounds.extend(new google.maps.LatLng(loc.LATITIUDE, loc.LONGITUDE));
+			    markers.push(createMarker(loc, amap));
 		   }
+		   amap.fitBounds(bounds);
 	   })
 	}
 
-	function createMarker(obj, amap, markers) {
+	function createMarker(obj, amap) {
 		var marker = new google.maps.Marker({
 		    position: new google.maps.LatLng(obj.LATITIUDE, obj.LONGITUDE),
 		    map: amap,
@@ -36,7 +32,7 @@ app.factory('MapFactory', function($http)
 			infowindow.open(amap, marker);
 		});
 
-		markers.push(marker);
+		return marker;
 	}
 
 	return factory;
