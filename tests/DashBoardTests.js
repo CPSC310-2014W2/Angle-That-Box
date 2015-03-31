8219a37c2748cd359b56956e61b3c136ffe34962
Jimmy <jimmy.nassar@hotmail.com>
2015-03-31 14:23:47 -0700

    changes to some views and some testing

--- tests/DashBoardTests.js
+++ tests/DashBoardTests.js
@@ -1,32 +1,67 @@
-'use strict',
+  'use strict',
 
-describe('dashboardController', function(){
+  describe('dashboardController', function(){
 
-  beforeEach(angular.mock.module('app'));
+    beforeEach(angular.mock.module('app'));
 
-  it('Input to set to empty string when item added', angular.mock.inject(function($controller, $injector) {
+    it('Input to set to empty string when item added', angular.mock.inject(function($controller, $injector) {
 
-    var scope = {},
-        ctrl = $controller('DashboardCtrl', {$scope:scope});
-    expect( scope.checkboxes.length).toBe(0);
-  }));
+      var scope = {};
+      ctrl = $controller('dashboardController', {$scope:scope});
+      expect( scope.checkboxes.length).toBe(0);
+    }));
 
-  it('Input to set to empty string when item added', angular.mock.inject(function($controller, $injector) {
-    var scope = {},
-        ctrl = $controller('DashboardCtrl', {$scope:scope});
-    expect( scope.filterTypes.length).toBe(1);
-  }));
- 
-});
+    it('Input to set to empty string when item added', angular.mock.inject(function($controller, $injector) {
+      var scope = {};
+      ctrl = $controller('dashboardController', {$scope:scope});
+      expect( scope.filterTypes.length).toBe(1);
+    }));
 
-describe('dashboardFactory', function(){
+  });
 
-	beforeEach(angular.mock.module('app'));
+  describe('dashboardFactory', function(){
 
-	it('check list', angular.mock.inject(function($injector) {
-		var factory = $injector.get('DashboardFactory');
+  	beforeEach(angular.mock.module('app'));
 
-	expect(factory.getList().length).toBe(0);
-  }));
+  	it('check list', angular.mock.inject(function($injector) {
+  		var factory = $injector.get('DashboardFactory');
 
-})
+     expect(factory.getList().length).toBe(0);
+   }));
+
+    describe('$scope.sort', function() {
+
+      it('sort a list of items A-Z', fucntion(){
+        var $scope = {};
+        ctrl = $controller('dashboardController', {$scope:scope});
+        var $originalList = $scope.checkboxes;
+        // $scope.list = //reference a json object with 3 entries in it
+        $scope.checkboxes = shuffle($scope.checkboxes);
+
+        $scope.sort('+CULTURAL_SPACE_NAME');
+        var $result = $scope.checkboxes;
+        expect($originalList).toEqual($result);
+      })
+
+    })
+
+  })
+
+  function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
