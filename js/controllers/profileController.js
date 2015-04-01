1d29124748b7e9713e718b3593b3f45244b3bb75
Kim <kimnovosel@live.ca>
2015-04-01 13:14:20 -0700

    selected file is now uploaded to profile page

--- js/controllers/profileController.js
+++ js/controllers/profileController.js
@@ -7,9 +7,29 @@ app.controller("ProfileController", function($scope, $firebase, AuthFactory, Pro
    var factory = ProfileFactory; 
    $scope.userData = factory.getUserData();
 
-   $scope.logout = function() {
+   $scope.logout = function () {
       authFactory.logout();
    }
+
+   $scope.uploadPhoto = function (selectedFile) {
+
+     // get the first image 
+     if (selectedFile.files && selectedFile.files[0]) {
+            var filereader = new FileReader();
+
+            // image is restricted to 150x150 square
+            filereader.onload = function (e) {
+                $('#imageID')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(150);
+            };
+
+            // URl for the image
+            filereader.readAsDataURL(selectedFile.files[0]);
+        }
+  }
+
 
 });
 
