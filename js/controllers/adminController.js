11904eb060354c63bfcc6b31aca1a251878ce5d7
u1d8 <ryan.io.93@gmail.com>
2015-04-04 15:38:11 -0700

    Admin page upload now checks for required fields and adjusted main page css

--- js/controllers/adminController.js
+++ js/controllers/adminController.js
@@ -1,15 +1,49 @@
 app.controller('AdminCtrl', function($scope) {
 
+	var requiredFields = ["CULTURAL_SPACE_NAME","WEBSITE","TYPE","PRIMARY_USE","ADDRESS","LOCAL_AREA","OWNERSHIP",
+	"SQUARE_FEET","NUMBER_OF_SEATS","ACTIVE_SPACE","LONGITUDE","LATITIUDE"]
+
 	$scope.upload = function() {
       var data = null;
       var file = $("#csvfile")[0].files[0];
+
+      if (file == undefined) {
+	      alert("Please choose a file");
+	      return
+      }
+
+	  //begin reading csv file
       var filereader = new FileReader();
       filereader.readAsText(file);
       filereader.onload = function(event) {
           var csvData = event.target.result;
           var objs = $.csv.toObjects(csvData);
-          console.log(objs);
+          
+          //check if object has required fields
+          var check = checkFields(objs[0]);
+          if (check) {
+	          console.log("Uploading file");
+          }
+
       }
+   }
+
+   var checkFields = function(data) {
+	   if (data == undefined) {
+		   alert("No objects exist");
+		   return false;
+
+	   } else {
+	   		//check that each field is present
+		   for (s in requiredFields) {
+			   if (!data.hasOwnProperty(requiredFields[s])) {
+				   alert(requiredFields[s] + " field does not exist");
+				   return false;
+			   }
+		   }
+
+		   return true;
+	   }
    }
 
    var parseData = function() {
