e603e1400e8f0d9425f45b34ab961099a2d0eb67
u1d8 <ryan.io.93@gmail.com>
2015-04-04 21:18:19 -0700

    Upload in admin page can upload locations to firebase, currently using different database url

--- js/controllers/adminController.js
+++ js/controllers/adminController.js
@@ -1,7 +1,12 @@
-app.controller('AdminCtrl', function($scope) {
+app.controller('AdminCtrl', function($scope, $firebase, $http) {
 
-	var requiredFields = ["CULTURAL_SPACE_NAME","WEBSITE","TYPE","PRIMARY_USE","ADDRESS","LOCAL_AREA","OWNERSHIP",
+	var url = "https://radiant-torch-6582.firebaseio.com/";
+    var ref = new Firebase(url);
+
+    var requiredFields = ["CULTURAL_SPACE_NAME","WEBSITE","TYPE","PRIMARY_USE","ADDRESS","LOCAL_AREA","OWNERSHIP",
 	"SQUARE_FEET","NUMBER_OF_SEATS","ACTIVE_SPACE","LONGITUDE","LATITIUDE"]
+
+	var locRef = ref.child("locations");
 
 	$scope.upload = function() {
       var data = null;
@@ -22,7 +27,7 @@ app.controller('AdminCtrl', function($scope) {
           //check if object has required fields
           var check = checkFields(objs[0]);
           if (check) {
-	          console.log("Uploading file");
+	          pushData(objs);
           }
 
       }
@@ -46,16 +51,31 @@ app.controller('AdminCtrl', function($scope) {
 	   }
    }
 
-   var parseData = function() {
-      $http.get('data/CulturalSpaces.csv').success(function(data) {
-         var objs = $.csv.toObjects(data);
-         var locations = {};
-         objs.forEach(function(item, i) {
-            item.LATITIUDE = parseInt(item.LATITIUDE);
-            item.LONGITUDE = parseInt(item.LONGITUDE);
-            var key = "loc" + parseInt(i);
-            locations[key] = item;
-         })
-      })
+   var pushDataBegin = function(newData) {
+		var fbData = $firebase(locRef).$asArray();
+		fbData.$loaded().then(function() {
+			pushData(fbData, list);
+		})
    }
+
+   var pushData = function(newData, fbData) {
+     newData.forEach(function(newItem, i) {
+     	var itemNotPresent = true;
+
+		for (j in fbData) {
+			if (fbData[j].CULTURAL_SPACE_NAME == newItem.CULTURAL_SPACE_NAME) {
+				itemNotPresent = false;
+     			break;
+			}
+		}
+
+     	if (itemNotPresent) {
+     		newItem.LATITIUDE = parseFloat(newItem.LATITIUDE);
+	        newItem.LONGITUDE = parseFloat(newItem.LONGITUDE);
+	        var key = "loc" + parseFloat(i);
+	        locRef.child(key).set(newItem);
+     	}
+     })
+   }
+
 })
